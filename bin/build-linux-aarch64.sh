55ae83778ad61bb0a6dc3266bf9cdad93bbe27a4
Jay Bonthius <jaybonthius@gmail.com>
2026-03-19 09:55:12 -0700

    use archive.debian.org for EOL debian 10 apt sources

--- bin/build-linux-aarch64.sh
+++ bin/build-linux-aarch64.sh
@@ -9,4 +9,4 @@ docker run --rm \
        -v "$(pwd)":"$(pwd)" \
        -w "$(pwd)" \
        debian:10 \
-       bash -c 'apt update && apt install -y build-essential cmake && mkdir -p brotli/out && cd brotli/out && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" -DBUILD_SHARED_LIBS=ON .. && cmake --build . --config Release --target install && find "$PREFIX"/lib -name "libbrotli*.so.*" -not -type l -exec strip {} +'
+       bash -c 'sed -i "s|deb.debian.org|archive.debian.org|g" /etc/apt/sources.list && sed -i "/buster-updates/d" /etc/apt/sources.list && apt update && apt install -y build-essential cmake && mkdir -p brotli/out && cd brotli/out && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" -DBUILD_SHARED_LIBS=ON .. && cmake --build . --config Release --target install && find "$PREFIX"/lib -name "libbrotli*.so.*" -not -type l -exec strip {} +'
