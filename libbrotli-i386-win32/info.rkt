34caccf52e64f30c45e26870bde3ace708475800
Jay Bonthius <jaybonthius@gmail.com>
2026-03-19 09:44:12 -0700

    fix versioning

--- libbrotli-i386-win32/info.rkt
+++ libbrotli-i386-win32/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define license 'MIT)
-(define version "1.0.0")
+(define version "1.0")
 (define deps '("base"))
 (define copy-foreign-libs '("libbrotlicommon.dll" "libbrotlienc.dll" "libbrotlidec.dll"))
 (define install-platform #rx"win32.i386")
