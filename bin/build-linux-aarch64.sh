9c049f56815f3d2b48cfca0cd93874128637ae5d
Jay Bonthius <jaybonthius@gmail.com>
2026-03-19 09:49:21 -0700

    build on debian 10 instead of 11 for broader compatibility

--- bin/build-linux-aarch64.sh
+++ bin/build-linux-aarch64.sh
@@ -8,5 +8,5 @@ docker run --rm \
        -e PREFIX="$PREFIX" \
        -v "$(pwd)":"$(pwd)" \
        -w "$(pwd)" \
-       debian:11 \
+       debian:10 \
        bash -c 'apt update && apt install -y build-essential cmake && mkdir -p brotli/out && cd brotli/out && cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="$PREFIX" -DBUILD_SHARED_LIBS=ON .. && cmake --build . --config Release --target install && find "$PREFIX"/lib -name "libbrotli*.so.*" -not -type l -exec strip {} +'
